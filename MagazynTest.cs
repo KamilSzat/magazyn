8709ddba24d59bc26fd2cc5eecf392db13c3e691
KamilSzat <uT_3776@zsp3zamosc.pl>
2025-05-28 10:13:57 +0200

    Kartkówka magazyn

---
+++ MagazynTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazyn
+{
+    [TestClass]
+
+    public class MagazynTest
+    {
+        [TestMethod]
+        public void Dodanie_produktow()
+        {
+            var magazyn = new Magazyn();
+            magazyn.dodajProdukt("Jajka", 30);
+            Assert.IsTrue(magazyn.zdejmijProdukt("Jajka", 15));
+            Assert.AreEqual(15, magazyn.sprawdzStan("Jajka"));
+        }
+
+        [TestMethod]
+        public void Zdejmowanie_produktow()
+        {
+            var magazyn = new Magazyn();
+            magazyn.dodajProdukt("Jajka", 20);
+            Assert.IsTrue(magazyn.zdejmijProdukt("Jajka", 15));
+            Assert.AreEqual(5, magazyn.sprawdzStan("Jajka"));
+        }
+        [TestMethod]
+        public void Zdejmowanie_produktowZaDuz0()
+        {
+            var magazyn = new Magazyn();
+            magazyn.dodajProdukt("Jajka", 10);
+            Assert.IsFalse(magazyn.zdejmijProdukt("Jajka", 20));
+            Assert.AreEqual(10, magazyn.sprawdzStan("Jajka"));
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazyn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Magazyn magazyn = new Magazyn();
+            magazyn.dodajProdukt("Mleko", 5);
+            magazyn.dodajProdukt("Chleb", 10);
+            magazyn.dodajProdukt("Jajka", 50);
+
+
+            Console.WriteLine($"Stan ilości mleka w magazynie: {magazyn.sprawdzStan("Mleko")}");
+            Console.WriteLine($"Stan ilości jaj w magazynie: {magazyn.sprawdzStan("Jajka")}");
+            Console.WriteLine($"Stan ilości chleba w magazynie: {magazyn.sprawdzStan("Chleb")}\n");
+
+            magazyn.zdejmijProdukt("Chleb", 3);
+            magazyn.zdejmijProdukt("Mleko", 2);
+            magazyn.zdejmijProdukt("Mleko1", 3);
+            magazyn.zdejmijProdukt("Jajka", 20);
+            Console.WriteLine($"Po zdjęciu 3 sztuk chleba: {magazyn.sprawdzStan("Chleb")}"); 
+            Console.WriteLine($"Po zdjęciu 20 sztuk jajek: {magazyn.sprawdzStan("Jajka")}"); 
+            Console.WriteLine($"Po zdjęciu 2 butelek mleka: {magazyn.sprawdzStan("Mleko")}\n" + $"oraz zdjęto 3 butelki mleka: {magazyn.sprawdzStan("Mleko1")}"); 
+
+
+        }
+    }
+}
